746a3f5633efeee46d2a8a8c28fd8ecc9eec12ba
Yuezhou <lujc515@163.com>
2018-11-20 17:19:36 +0900

    Implement Note Date

--- js/retain.js
+++ js/retain.js
@@ -20,7 +20,8 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                unixTimeSubmitted: Date.now()
             });
             view.render();
         },
@@ -48,16 +49,41 @@ $(function(){
             });
             view.render();
         },
-        render: function(){
+        render: function() {
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
+                // Create a new JavaScript Date object based on the unix timestamp (has to be in milliseconds to use Date()).
+                var realTime = new Date(note.unixTimeSubmitted);
+                // Year part from the timestamp
+                var year = realTime.getFullYear();
+                // Month part from the timestamp !January is 0, February is 1, and so on!
+                var monthIndex = realTime.getMonth();
+                const monthNames = [
+                    'January', 'February', 'March',
+                    'April', 'May', 'June', 'July',
+                    'August', 'September', 'October',
+                    'November', 'December',
+                ];
+                const month = monthNames[monthIndex];
+                // Day part from the timestamp
+                const day = realTime.getDate();
+                // Hours part from the timestamp
+                var hours = realTime.getHours();
+                // Minutes part from the timestamp
+                var minutes = "0" + realTime.getMinutes();
+                // Seconds part from the timestamp
+                var seconds = "0" + realTime.getSeconds();
+                // Will display time in 17:11:19 - November 20, 2018 format
+                var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + ' ' + `- ${month} ${day}, ${year}`;
+
                 htmlStr += '<li class="note">'+
+                        '<span class="note-date">' + formattedTime.toString() + '</span>' +
                         note.content +
                     '</li>';
             });
-            this.noteList.html( htmlStr );
+            this.noteList.html(htmlStr);
         }
     };
-
+    localStorage.clear();
     octopus.init();
 });
